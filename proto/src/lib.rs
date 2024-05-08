5e9041565b0de97e164e0115c667b15ca144612e
Søren Alsbjerg Hørup <sah@sah.dk>
2024-05-08 21:02:52 +0200

    wip protobuf

--- proto/src/lib.rs
+++ proto/src/lib.rs
@@ -2,5 +2,23 @@ mod proto {
     include!(concat!(env!("OUT_DIR"), "/messages.rs"));
 }
 
+impl netcode::SerializableMessage for proto::Message {
+    fn to_bytes(&self) -> Result<Vec<u8>, ()> {
+        Ok(prost::Message::encode_to_vec(self))
+    }
+
+    fn from_bytes(bytes:&[u8]) -> Result<Self, ()> {
+        prost::Message::decode(bytes).map_err(|_|())
+    }
+
+    fn to_json(&self) -> Result<String, ()> {
+        panic!("not supported!");
+    }
+
+    fn from_json(_json:&str) -> Result<Self, ()> {
+        panic!("not supported!")
+    }
+}
+
 pub use proto::*;
 pub use prost;
